53e83ffb132b3cced4ed7445bbd851e24b84228c
Mehmet Ali SAHINOGLLARI <mehmets@sistek.local>
2017-11-29 23:51:26 +0300

    Add Register Page

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service/auth-service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 /**
  * Generated class for the RegisterPage page.
@@ -14,12 +16,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
+  createSuccess = false;
+  registerCredentials = { email: '', password: '' }
+  res = { success: 'SUCCESS', fail: 'FAIL', error: 'ERROR' }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public nav: NavController, private auth: AuthService, private alertCtrl: AlertController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+  public register() {
+    this.auth.register(this.registerCredentials).subscribe(success => {
+      if (success) {
+        this.createSuccess = true;
+        this.showPopup(this.res.success, 'Account Created');
+      } else {
+        this.showPopup(this.res.fail, "Problem creating Account");
+      }
+    }, error => {
+      this.showPopup(this.res.error, error);
+    });
+  }
+
+  showPopup(title, text) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: text,
+      buttons: [{
+        text: 'OK',
+        handler: data => {
+          if(this.createSuccess) {
+            this.nav.popToRoot();
+          }
+        }
+      }]
+    });
+    alert.present();
   }
 
 }
