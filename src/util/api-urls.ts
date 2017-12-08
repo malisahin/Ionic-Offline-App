0547b9d71b29750089175aa428ce830c595c7f6b
Mehmet Ali SAHINOGLLARI <mehmets@sistek.local>
2017-12-08 05:27:04 +0300

    Profiling Customers

--- src/util/api-urls.ts
+++ src/util/api-urls.ts
@@ -8,7 +8,7 @@ export class Api {
 
 
 
-    tokenUrl: string = "http://10.10.10.26:7005/sos-security-service/oauth/token?grant_type=password&client_id=sos-api-enduser-mobile-client&client_secret=somesecret&username=ECAMERKEZ&password=13131313";
+    tokenUrl: string = "";
 
     loginUrl: string = "";
 }
