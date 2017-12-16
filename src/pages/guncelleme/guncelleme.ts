84e56f6627b926918ffbd1f435bdb86a8f3300ee
malisahin <mehmetalisahinogullari@gmail.com>
2017-12-16 16:10:00 +0300

    Bind API with Views

--- src/pages/guncelleme/guncelleme.ts
+++ src/pages/guncelleme/guncelleme.ts
@@ -2,9 +2,17 @@
  * @author malisahin
  * @email mehmetalisahinogullari@gmail.com
 */
+
+
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MalzemeIscilikProvider } from '../../providers/malzeme-iscilik/malzeme-iscilik';
+import { UrunProvider } from '../../providers/urun/urun';
+import { UrunAnaGrpProvider } from '../../providers/urun-ana-grp/urun-ana-grp';
+import { UrunIscilikProvider } from '../../providers/urun-iscilik/urun-iscilik';
+import { UrunMalzemeProvider } from '../../providers/urun-malzeme/urun-malzeme';
+import { FiyatProvider } from '../../providers/fiyat/fiyat';
+import { IslemArizaIscilikProvider } from '../../providers/islem-ariza-iscilik/islem-ariza-iscilik';
 
 
 @IonicPage()
@@ -18,12 +26,61 @@ export class GuncellemePage {
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public mlzIscProvider: MalzemeIscilikProvider) {
+    public mlzIscProvider: MalzemeIscilikProvider,
+    private urunProvider: UrunProvider,
+    private urunAnaGrpProvider: UrunAnaGrpProvider,
+    private urunIscilikProvider: UrunIscilikProvider,
+    private urunMalzemeProvider: UrunMalzemeProvider,
+    private islemArizaIscilikProvider: IslemArizaIscilikProvider,
+    private fiyatProvider: FiyatProvider) {
+  }
+
+  downloadUrunler() {
+    this.urunProvider.downloadUrunler("-1", 0).subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  downloadUrunAnaGrup() {
+    this.urunAnaGrpProvider.downloadUrunAnaGrup("-1").subscribe(res => {
+      console.log(res);
+    });
   }
 
   downloadUrunIscilik() {
-    this.mlzIscProvider.urunIscilikGuncelle("-1", 0);
+    this.urunIscilikProvider.downloadUrunIscilik("-1", 0).subscribe(res => {
+      console.log(res);
+    });
   }
+
+  downloadUrunMalzeme() {
+    this.urunMalzemeProvider.downloadUrunMalzeme("-1", 0).subscribe(res => {
+      console.log(res);
+    });
+
+  }
+
+  downloadIslemArizaIscilik() {
+    this.islemArizaIscilikProvider.downloadIslemArizaIscilik("-1", 0).subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  downloadMalzemeFiyat() {
+    this.fiyatProvider.downloadMalzemeFiyat("-1", 0).subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  downloadIscilikFiyat() {
+    this.fiyatProvider.downloadIscilikFiyat("-1", 0).subscribe(res => {
+      console.log(res);
+    });
+  }
+
+
+
+
 
 
 }
