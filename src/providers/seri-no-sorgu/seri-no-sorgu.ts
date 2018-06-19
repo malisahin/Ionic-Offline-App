c00aa0f361b28c1de07fc58fc41f5527b3d48b61
malisahin <mehmetalisahinogullari@gmail.com>
2018-06-19 18:01:45 +0300

    Seri No Sorgu

---
+++ src/providers/seri-no-sorgu/seri-no-sorgu.ts
@@ -0,0 +1,37 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {ApiProvider} from "../api/api";
+import {UtilProvider} from "../util/util";
+import {TokenProvider} from "../token/token";
+import {LoggerProvider} from "../logger/logger";
+
+/**
+ * @author : mali.sahin
+ * @since  : 18.06.2018
+ */
+
+@Injectable()
+export class SeriNoSorguProvider {
+
+  constructor(public http: HttpClient,
+              private  api: ApiProvider,
+              private  util: UtilProvider,
+              private  tokenProvider: TokenProvider,
+              private  logger: LoggerProvider) {
+  }
+
+
+  public async fetchData(mamKod: string): Promise<any> {
+    if (this.util.isNotEmpty(mamKod)) {
+      let url = this.api.seriNoSorguUrl(mamKod);
+      this.logger.warn(url);
+      let header = await this.tokenProvider.callTokenAndGetHeader();
+      let res = await this.http.get(url, {headers: header}).toPromise();
+      return new Promise((resolve, reject) => {
+        resolve(res);
+      })
+    } else {
+      this.util.error("Seri No alanı sorgu için zorunludur.");
+    }
+  }
+}
