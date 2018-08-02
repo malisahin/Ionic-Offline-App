5c49007ab5d199fc92946a07be71395d2c6b98d7
malisahin <mehmetalisahinogullari@gmail.com>
2018-08-02 19:16:07 +0300

    Barcode Settings

--- src/providers/util-plugin/util-plugin.ts
+++ src/providers/util-plugin/util-plugin.ts
@@ -26,15 +26,14 @@ export class UtilPlugin {
   scanBarcode() {
     this.barcodeScanner.scan(
       {
-        preferFrontCamera : true, // iOS and Android
+        preferFrontCamera : false, // iOS and Android
         showFlipCameraButton : true, // iOS and Android
         showTorchButton : true, // iOS and Android
         torchOn: true, // Android, launch with the torch switched on (if available)
-        saveHistory: true, // Android, save scan history (default false)
         prompt : "Place a barcode inside the scan area", // Android
-        resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-        formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-        orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+        resultDisplayDuration: 2500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+        formats : "CODE_128, CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+        //orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
         disableAnimations : true, // iOS
         disableSuccessBeep: false // iOS and Android
       }
