1544b33e5c443f69a3b899a0ca782a4da963578e
Sahinogullari <mehmetalisahinogullari@hotmail.com>
2018-09-12 17:36:39 +0300

    Profil Bilgilerinin Düzenlenmesi

--- src/entities/profil.ts
+++ src/entities/profil.ts
@@ -8,92 +8,37 @@ import { Constants } from "./Constants";
 export class Profil {
 
   public static getActiveProfil() {
-    return Profil.getEcaTestDomain();
+    return Profil.getCustomer1();
   }
 
   public static getOrgKod() {
     return Profil.getActiveProfil().orgKod;
   }
 
-  private static getlocalDev(): Domain {
-    let domain = new Domain();
-    domain.orgKod = Constants.ORG_KODS.SOS;
-    domain.kod = 'SECRET';
-    domain.name = 'Development';
-    domain.domainUrl = "http://10.10.10.151:7001";
-    domain.securityUrl = "http://sos.ecaservis.com.tr:7003";
-    domain.webappurl = "http://sos.ecaservis.com.tr";
-    return domain;
-  }
-
-  private static getlocalTest() {
-    let domain = new Domain();
-    domain.orgKod = Constants.ORG_KODS.SOS;
-    domain.kod = 'SECRET';
-    domain.name = 'Development';
-    domain.domainUrl = "http://10.10.10.26:7005";
-    domain.securityUrl = "http://10.10.10.26:7005";
-    domain.webappurl = "http://10.10.10.26:7005";
-    return domain;
-  }
-
-  private static getBaymakTestVPNDomain() {
-    let domain = new Domain();
-    domain.orgKod = Constants.ORG_KODS.BAY;
-    domain.kod = 'SECRET';
-    domain.name = 'Development';
-    domain.domainUrl = "http://172.24.1.10:7005";
-    domain.securityUrl = "http://172.24.1.10:7005";
-    domain.webappurl = "http://172.24.1.10:7005";
-    return domain;
-  }
-
-  private static getBaymakTestDomain() {
-    let domain = new Domain();
-    domain.orgKod = Constants.ORG_KODS.BAY;
-    domain.kod = 'SECRET';
-    domain.name = 'Development';
-    domain.domainUrl = "http://sos.baymak.com.tr:7008";
-    domain.securityUrl = "http://sos.baymak.com.tr:7008";
-    domain.webappurl = "http://sos.baymak.com.tr:7008";
-    return domain;
-  }
-
-  private static getBaymakProdDomain() {
-    let domain = new Domain();
-    domain.orgKod = Constants.ORG_KODS.BAY;
-    domain.kod = 'SECRET';
-    domain.name = 'Development';
-    domain.domainUrl = "http://sos.baymak.com.tr:7006";
-    domain.securityUrl = "http://sos.baymak.com.tr:7006";
-    domain.webappurl = "http://sos.baymak.com.tr:7005";
-    return domain;
-  }
-
-  private static getEcaTestDomain() {
-    let domain = new Domain();
-    domain.orgKod = Constants.ORG_KODS.ECA;
-    domain.kod = 'SECRET';
-    domain.name = 'Development';
-    domain.domainUrl = "http://sos.ecaservis.com.tr:7003";
-    domain.securityUrl = "http://sos.ecaservis.com.tr:7003";
-    domain.webappurl = "http://sos.ecaservis.com.tr";
-    return domain;
-  }
-
-
-  private static getEcaProdDomain() {
-    let domain = new Domain();
-    domain.orgKod = Constants.ORG_KODS.ECA;
-    domain.kod = 'SECRET';
-    domain.name = 'Development';
-    domain.domainUrl = "http://sos.ecaservis.com.tr:7009";
-    domain.securityUrl = "http://sos.ecaservis.com.tr:7009";
-    domain.webappurl = "http://sos.ecaservis.com.tr:7009";
-    return domain;
-  }
 
   private static getCustomer3() {
+    let domain = new Domain();
+    domain.orgKod = "SAHIN";
+    domain.kod = 'SECRET';
+    domain.name = 'Development';
+    domain.domainUrl = "";
+    domain.securityUrl = "";
+
+    return domain;
+  }
+
+  private static getCustomer2() {
+    let domain = new Domain();
+    domain.orgKod = "SAHIN";
+    domain.kod = 'SECRET';
+    domain.name = 'Development';
+    domain.domainUrl = "";
+    domain.securityUrl = "";
+
+    return domain;
+  }
+
+  private static getCustomer1() {
     let domain = new Domain();
     domain.orgKod = "SAHIN";
     domain.kod = 'SECRET';
